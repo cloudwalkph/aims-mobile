0aaba4f3847ab33f2d3fcce428f733e6e391efb2
Nina Kristel Cheng <nkmcheng@live.com>
2017-07-04 13:36:01 +0800

    added login activity

--- app/src/main/java/com/cloudwalkdigital/aims/LoginActivity.java
+++ app/src/main/java/com/cloudwalkdigital/aims/LoginActivity.java
@@ -1,13 +1,31 @@
 package com.cloudwalkdigital.aims;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+
+import com.cloudwalkdigital.aims.projectselection.ProjectSelectionActivity;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class LoginActivity extends AppCompatActivity {
+
+    @BindView(R.id.btn_log_in) Button mBtnLogin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        ButterKnife.bind(this);
+    }
+
+    @OnClick(R.id.btn_log_in)
+    public void doLogin(Button button) {
+        Intent intent = new Intent(LoginActivity.this, ProjectSelectionActivity.class);
+        startActivity(intent);
     }
 }
