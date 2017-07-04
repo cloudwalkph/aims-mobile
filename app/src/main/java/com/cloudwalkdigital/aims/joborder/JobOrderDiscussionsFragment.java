2a7f1fef0384f7676e135f1e0b486d68379049a7
Nina Kristel Cheng <nkmcheng@live.com>
2017-07-04 17:57:41 +0800

    add chat and patter

--- app/src/main/java/com/cloudwalkdigital/aims/joborder/JobOrderDiscussionsFragment.java
+++ app/src/main/java/com/cloudwalkdigital/aims/joborder/JobOrderDiscussionsFragment.java
@@ -1,13 +1,24 @@
 package com.cloudwalkdigital.aims.joborder;
 
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.v4.app.Fragment;
+import android.support.v4.content.ContextCompat;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.cloudwalkdigital.aims.R;
+import com.github.bassaer.chatmessageview.models.Message;
+import com.github.bassaer.chatmessageview.models.User;
+import com.github.bassaer.chatmessageview.utils.ChatBot;
+import com.github.bassaer.chatmessageview.views.ChatView;
+
+import java.util.Random;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -15,6 +26,9 @@ import com.cloudwalkdigital.aims.R;
  * create an instance of this fragment.
  */
 public class JobOrderDiscussionsFragment extends Fragment {
+
+    private ChatView mChatView;
+
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
@@ -48,17 +62,83 @@ public class JobOrderDiscussionsFragment extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
+
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_job_order_discussions, container, false);
+        View view = inflater.inflate(R.layout.fragment_job_order_discussions, container, false);
+
+        //User id
+        int myId = 0;
+        //User icon
+        Bitmap myIcon = BitmapFactory.decodeResource(getResources(), R.drawable.face_2);
+        //User name
+        String myName = "Michael";
+
+        int yourId = 1;
+        Bitmap yourIcon = BitmapFactory.decodeResource(getResources(), R.drawable.face_1);
+        String yourName = "Emily";
+
+        final User me = new User(myId, myName, myIcon);
+        final User you = new User(yourId, yourName, yourIcon);
+
+        mChatView = (ChatView) view.findViewById(R.id.chat_view);
+
+        //Set UI parameters if you need
+        mChatView.setRightBubbleColor(ContextCompat.getColor(getContext(), R.color.green500));
+        mChatView.setLeftBubbleColor(Color.WHITE);
+        mChatView.setBackground(ContextCompat.getDrawable(getContext(), R.drawable.background_pattern));
+        mChatView.setSendButtonColor(ContextCompat.getColor(getContext(), R.color.cyan900));
+        mChatView.setSendIcon(R.drawable.ic_action_send);
+        mChatView.setRightMessageTextColor(Color.WHITE);
+        mChatView.setLeftMessageTextColor(Color.BLACK);
+        mChatView.setUsernameTextColor(Color.WHITE);
+        mChatView.setSendTimeTextColor(Color.WHITE);
+        mChatView.setDateSeparatorColor(Color.WHITE);
+        mChatView.setInputTextHint("Type a message...");
+        mChatView.setMessageMarginTop(15);
+        mChatView.setMessageMarginBottom(0);
+
+        //Click Send Button
+        mChatView.setOnClickSendButtonListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //new message
+                Message message = new Message.Builder()
+                        .setUser(me)
+                        .setRightMessage(true)
+                        .setMessageText(mChatView.getInputText())
+                        .hideIcon(true)
+                        .build();
+                //Set to chat view
+                mChatView.send(message);
+                //Reset edit text
+                mChatView.setInputText("");
+
+                //Receive message
+                final Message receivedMessage = new Message.Builder()
+                        .setUser(you)
+                        .setRightMessage(false)
+                        .setMessageText(ChatBot.talk(me.getName(), message.getMessageText()))
+                        .build();
+
+                // This is a demo bot
+                // Return within 3 seconds
+                int sendDelay = (new Random().nextInt(4) + 1) * 1000;
+                new Handler().postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        mChatView.receive(receivedMessage);
+                    }
+                }, sendDelay);
+            }
+
+        });
+
+        return view;
     }
 
 }
