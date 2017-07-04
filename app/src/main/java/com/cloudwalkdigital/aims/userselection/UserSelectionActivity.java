836355d09dea9fff9a8f7f62a17701a56f5bd8e3
Nina Kristel Cheng <nkmcheng@live.com>
2017-07-04 16:42:42 +0800

    validate to user selection

---
+++ app/src/main/java/com/cloudwalkdigital/aims/userselection/UserSelectionActivity.java
@@ -0,0 +1,15 @@
+package com.cloudwalkdigital.aims.userselection;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.cloudwalkdigital.aims.R;
+
+public class UserSelectionActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user_selection);
+    }
+}
