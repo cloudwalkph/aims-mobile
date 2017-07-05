b8c0c8b8d5647ea75da70947ec9eec8973cdeda8
Alleo Indong <freedom_war550@live.com.ph>
2017-07-05 08:55:49 +0800

    added schedule model

---
+++ app/src/main/java/com/cloudwalkdigital/aims/data/model/Schedule.java
@@ -0,0 +1,48 @@
+package com.cloudwalkdigital.aims.data.model;
+
+import com.google.gson.annotations.SerializedName;
+
+/**
+ * Created by alleoindong on 7/5/17.
+ */
+
+public class Schedule {
+    private Integer id;
+    private Venue venue;
+    private String remarks;
+    @SerializedName("jo_datetime")
+    private String joDatetime;
+    private String status;
+
+    public Schedule() {
+    }
+
+    public Schedule(Integer id, Venue venue, String remarks,
+                    String joDatetime, String status) {
+        this.id = id;
+        this.venue = venue;
+        this.remarks = remarks;
+        this.joDatetime = joDatetime;
+        this.status = status;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public Venue getVenue() {
+        return venue;
+    }
+
+    public String getRemarks() {
+        return remarks;
+    }
+
+    public String getJoDatetime() {
+        return joDatetime;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+}
