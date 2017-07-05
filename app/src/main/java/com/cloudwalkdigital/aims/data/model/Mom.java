b21b40400128fec7778340b775b4df7176603696
Alleo Indong <freedom_war550@live.com.ph>
2017-07-05 08:55:40 +0800

    added mom model

---
+++ app/src/main/java/com/cloudwalkdigital/aims/data/model/Mom.java
@@ -0,0 +1,77 @@
+package com.cloudwalkdigital.aims.data.model;
+
+import com.google.gson.annotations.SerializedName;
+
+/**
+ * Created by alleoindong on 7/5/17.
+ */
+
+public class Mom {
+    private Integer id;
+    private String agenda;
+    @SerializedName("date_and_time")
+    private String dateAndTime;
+    private String location;
+    private String attendees;
+    @SerializedName("campaign_overview")
+    private String campaignOverview;
+    @SerializedName("activations_flow")
+    private String activationsFlow;
+    @SerializedName("next_step_deliverables")
+    private String nextStepDeliverables;
+    @SerializedName("other_details")
+    private String otherDetails;
+
+    public Mom() {
+    }
+
+    public Mom(Integer id, String agenda, String dateAndTime, String location, String attendees,
+               String campaignOverview, String activationsFlow, String nextStepDeliverables,
+               String otherDetails) {
+        this.id = id;
+        this.agenda = agenda;
+        this.dateAndTime = dateAndTime;
+        this.location = location;
+        this.attendees = attendees;
+        this.campaignOverview = campaignOverview;
+        this.activationsFlow = activationsFlow;
+        this.nextStepDeliverables = nextStepDeliverables;
+        this.otherDetails = otherDetails;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public String getAgenda() {
+        return agenda;
+    }
+
+    public String getDateAndTime() {
+        return dateAndTime;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public String getAttendees() {
+        return attendees;
+    }
+
+    public String getCampaignOverview() {
+        return campaignOverview;
+    }
+
+    public String getActivationsFlow() {
+        return activationsFlow;
+    }
+
+    public String getNextStepDeliverables() {
+        return nextStepDeliverables;
+    }
+
+    public String getOtherDetails() {
+        return otherDetails;
+    }
+}
