984618e42319c21f0df12a583ce6209b3a46de83
Alleo Indong <freedom_war550@live.com.ph>
2017-07-05 08:56:02 +0800

    added venue model

---
+++ app/src/main/java/com/cloudwalkdigital/aims/data/model/Venue.java
@@ -0,0 +1,38 @@
+package com.cloudwalkdigital.aims.data.model;
+
+/**
+ * Created by alleoindong on 7/5/17.
+ */
+
+public class Venue {
+    private Integer id;
+    private String category;
+    private String subcategory;
+    private String venue;
+
+    public Venue() {
+    }
+
+    public Venue(Integer id, String category, String subcategory, String venue) {
+        this.id = id;
+        this.category = category;
+        this.subcategory = subcategory;
+        this.venue = venue;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public String getSubcategory() {
+        return subcategory;
+    }
+
+    public String getVenue() {
+        return venue;
+    }
+}
